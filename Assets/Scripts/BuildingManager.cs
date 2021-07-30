0f541e92597b4ac81a17f942dc8f9864a62e7dc8
Sourav Chatterjee <sourav.chatterjee@lightheart.games>
2021-07-30 22:42:20 +0530

    Added ability to place buildings at clicked location.

---
+++ Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,28 @@
+﻿using Unity.Mathematics;
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour
+{
+    [SerializeField] private Transform pfWoodHarvestor;
+    private Camera _camera;
+    
+    private void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(pfWoodHarvestor, GetMousePosition(), Quaternion.identity);
+        }
+    }
+
+    private Vector3 GetMousePosition()
+    {
+        Vector3 mouseWorldPosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPosition.z = 0f;
+        return mouseWorldPosition;
+    }
+}
